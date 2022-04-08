9aac03395e0c5fb918d58b014f4455e687478dd5
Chineta Adinnu <52791481+Netacci@users.noreply.github.com>
2022-04-08 20:19:56 +0100

    fix color issue

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -5,7 +5,7 @@ const Tabs = ({ filterProducts, data, setFilter }) => {
     <>
       <div className='flex flex-wrap mt-5 border-b border-gray-200 dark:border-gray-700 justify-center'>
         <button
-          className='mr-2  inline-block py-4 px-4 text-sm font-medium text-center transition duration-700 ease-out  hover:text-teal-600 hover:bg-gray-50  dark:bg-gray-800 dark:text-gray-500'
+          className='mr-2  inline-block py-4 px-4 text-sm font-medium text-center transition duration-700 ease-out  hover:text-teal-600 hover:bg-gray-50 dark:hover:text-gray-300  dark:hover:bg-gray-800 dark:text-gray-500'
           onClick={(e) => {
             e.preventDefault();
             setFilter(data);
