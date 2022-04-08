074c2091e79880cbce9db3d7f7c4a7025b326b6e
Chineta Adinnu <52791481+Netacci@users.noreply.github.com>
2022-04-08 20:44:19 +0100

    fix not found page message

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -4,10 +4,17 @@ import { Icon } from '@iconify/react';
 
 const ComingSoon = () => {
   return (
-    <div className='px-20 py-20 text-center'>
-      <div className=' flex place-items-center justify-center text-3xl font-bold'>
-        This page is not ready yet so
-        <Icon className='text-rose-800 ml-3 mr-3' icon='el:arrow-right' />
+    <div className=' md:p-20 p-7 text-center'>
+      <div className=' flex flex-col md:flex-row place-items-center justify-center text-3xl font-bold'>
+        This page is not ready yet
+        <Icon
+          className='text-rose-800 ml-3 mr-3 hidden md:block'
+          icon='el:arrow-right'
+        />
+        <Icon
+          className='text-rose-800 ml-3 mr-3 mt-4 mb-4 text-4xl md:hidden'
+          icon='entypo:arrow-down'
+        />
         <Link
           to='/products'
           className=' text-white bg-rose-700 hover:bg-rose-800 focus:ring-4 focus:ring-rose-300 font-medium transition-all ease-in duration-300  rounded-lg text-sm px-5 py-2.5 text-center'
